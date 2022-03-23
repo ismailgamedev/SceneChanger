a017b298575efbdbcd1d0227fd3ab45aaf6b3736
Ismail Filipov <64804972+ismailgamedev@users.noreply.github.com>
2022-03-23 23:01:00 +0100

    Delete addons directory

--- addons/change_scene/Fade.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-func _play():
-	get_node("AnimationPlayer").play("fade")
-
-func _play_backward():
-	get_node("AnimationPlayer").play_backwards("fade")
--- addons/change_scene/example/Scene1.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		ChangeScene.change_scene("res://addons/change_scene/example/Scene2.tscn")
